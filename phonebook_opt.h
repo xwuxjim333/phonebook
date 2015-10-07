8316e4109e4642c5475c4fa92137c599411c9c0e
xwuxjim333 <xwuxjim333@gmail.com>
2015-10-07 16:17:30 +0800

    optimizing version 2 (failed)
    Try to use binary search tree, but it gets worse.

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -6,7 +6,8 @@
 typedef struct __PHONE_BOOK_LASTNAME {
     char lastName[MAX_LAST_NAME_SIZE];
     struct __PHONE_BOOK_ENTRY *detail;
-    struct __PHONE_BOOK_LASTNAME *pNext;
+    struct __PHONE_BOOK_LASTNAME *pLeft;
+    struct __PHONE_BOOK_LASTNAME *pRight;
 } entry;
 
 typedef struct __PHONE_BOOK_ENTRY {
@@ -19,10 +20,9 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
-    struct __PHONE_BOOK_ENTRY *pNext;
+    //struct __PHONE_BOOK_ENTRY *pNext;
 } entryDetail;
 
-entry *findName(char lastname[], entry *pHead);
-entry *append(char lastName[], entry *e);
-
+entry *findName(char lastname[], entry *root);
+entry *append(char lastName[], entry *root);
 #endif
